e6c78ed546b0855e7a53f103e24e49f370c67713
Tirumal Rao <tirumal.rao@freecharge.com>
2021-03-25 14:28:17 +0530

    signup with attributes and custom attributes

--- initialize.js
+++ initialize.js
@@ -1,21 +1,39 @@
-const util = require('./lib/util');
+// Load Env from .env file
+require('dotenv').config();
 
-// Set initErrors
-let initErrors = false;
+// util
+const { breakLog, currentDateNTime, exitProcess, showInfo, showError, showWarnings } = require('./lib/util');
+
+// Initiate with current timestamp
+showInfo(`${currentDateNTime()} Initiating...\n\n`);
+
+// Stacks
 const errorStack = [];
+const warningStack = [];
 
 // Check required env variables
-if (!process.env.COGNITO_USER_POOL_ID) { errorStack.push('Missing env variable: COGNITO_USER_POOL_ID'); initErrors = true; }
-if (!process.env.COGNITO_CLIENT_ID) { errorStack.push('Missing env variable: COGNITO_CLIENT_ID'); initErrors = true; }
+if (!process.env.COGNITO_USER_POOL_ID) { errorStack.push('Missing env variable: COGNITO_USER_POOL_ID');}
+if (!process.env.COGNITO_CLIENT_ID) { errorStack.push('Missing env variable: COGNITO_CLIENT_ID');}
+if (!process.env.PORT) { warningStack.push('Missing env variable: PORT. Using default port 3008.');};
 
 // IF initErrors THEN
-if (initErrors) {
+if (errorStack.length > 0) {
   // Error messages from the stack
-  util.showInfo('Showing initial errors!\n\n')
+  showInfo('Encountered errors while initiating the application! Following are the list of errors.\n\n')
   for (let i = 0; i < errorStack.length; i++) {
-    util.showError(errorStack[i]);
+    showError(errorStack[i]);
   }
-  util.breakLog();
-  util.showInfo('Please fix above errors to start the application\n\n');
-  util.exitProcess();
+  breakLog();
+  showInfo('Please fix above errors to start the application\n\n');
+  exitProcess();
+} else {
+  showInfo('Successfully Initiated.\n');
+}
+
+// IF initWarnings THEN
+if (warningStack.length > 0) {
+  for (let i = 0; i < warningStack.length; i++) {
+    showWarnings(warningStack[i]);
+  }
+  breakLog();
 }
--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 require('./initialize');
 
 const express = require('express');
-const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3008;
 
 // Bodyparser
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
