7f6219943663123a9a7b011e63577fb8f0dd42cc
Tirumal Rao <tirumal.rao@freecharge.com>
2021-04-25 12:33:52 +0530

    resend confirmation link

--- routes/auth.js
+++ routes/auth.js
@@ -150,10 +150,10 @@ module.exports = app => {
 
         res.send(data);
       });
-    } catch ({ message }) {
+    } catch (err) {
       res.status(500).send({
         error: true,
-        messsage
+        messsage: err.message
       });
     }
   });
