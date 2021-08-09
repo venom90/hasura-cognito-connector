45d5d948feb7d0890e112b97f61cdfeee847afad
Tirumal Rao <tirumal.rao@freecharge.com>
2021-08-09 12:33:38 +0530

    :sparkles: Hasura Login Action

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,71 @@ const { cognitoidentityserviceprovider } = require('../lib/cognito-identity-serv
 
 
 module.exports = app => {
+  app.post('/auth/actions/login', (req, res) => {
+    try {
+      // Destructure username and password from request body
+      const { username, password } = req.body.input;
+
+      // Auth data
+      const authenticationData = {
+        Username: username,
+        Password: password
+      };
+
+      // Auth details for Cognito
+      const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+
+      // Cognito User
+      const cognitoUser = createCognitoUser(username);
+
+      // Authenticate with auth details
+      cognitoUser.authenticateUser(authenticationDetails, {
+        // Handle on success event of cognito auth user
+        onSuccess: async result => {
+
+          const params = { AccessToken: result.accessToken.jwtToken };
+          const userResult = await cognitoidentityserviceprovider.getUser(params).promise();
+
+          // Fetch all attributes
+          const attributes = userResult.UserAttributes;
+          // Fetch Hasura User id
+          const user_id = attributes.find(attribute => attribute.Name === process.env.USER_ID_ATTRIBUTE).Value;
+
+          // Send the response back to client in JSON
+          res.send({
+            message: '',
+            error: false,
+            success: true,
+            accessToken: result.accessToken.jwtToken,
+            refreshToken: result.refreshToken.token,
+            idToken: result.idToken,
+            user_id
+          });
+        },
+
+        // Handle on failure event of cognito auth user
+        onFailure: err => res.send({ 
+          message: err.message,
+          error: true,
+          success: false,
+          accessToken: '',
+          refreshToken: '',
+          idToken: '',
+          user_id: ''
+         })
+      });
+    } catch ({ message }) {
+      res.status(500).send({
+        message: message,
+        error: true,
+        success: false,
+        accessToken: '',
+        refreshToken: '',
+        idToken: '',
+        user_id: ''
+      });
+    }
+  })
   /**
    * @description Login user with username and password
    * @method POST
